cbc7258e13af083fccaa7dadf392fa49de496b34
pnabiniw <pnabiniw@gmail.com>
2023-10-05 16:24:32 +0545

    media

--- crud/urls.py
+++ crud/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import classroom, classroom_delete, classroom_update
+from .views import classroom, classroom_delete, classroom_update, \
+    student, add_student
 
 urlpatterns = [
+    path("student/", student, name="crud_student"),
+    path("add-student/", add_student, name="add_student"),
     path("class/delete/<int:id>/", classroom_delete, name="classroom_delete"),
     path("class/update/<int:id>/", classroom_update, name="classroom_update"),
     path("", classroom, name="crud_classroom")
--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import ClassRoom
+from .models import ClassRoom, Student, StudentProfile
 
 
 def classroom(request):
@@ -29,3 +29,27 @@ def classroom_update(request, id):
     classroom = ClassRoom.objects.get(id=id)
     return render(request, template_name="crud/classroom_update.html",
                   context={"title": "Classroom Update", "classroom": classroom})
+
+
+def student(request):
+    students = Student.objects.all()
+    return render(request, template_name="crud/student.html",
+                  context={"title": "Student", "students": students})
+
+
+def add_student(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        age = request.POST.get("age")
+        address = request.POST.get("address")
+        contact = request.POST.get("contact")
+        pp = request.FILES.get('pp')
+        std = Student.objects.create(name=name, email=email, age=age, classroom_id=1)
+        sp = StudentProfile.objects.create(contact=contact, address=address, student=std)
+        if pp:
+            sp.profile_picture = pp
+            sp.save()
+        return redirect("crud_student")
+    return render(request, template_name="crud/add_student.html",
+                  context={"title": "Add Student"})
