906f7ae4b7dcdabb26e429bf7a8411604c29f196
pnabiniw <pnabiniw@gmail.com>
2023-10-06 16:28:58 +0545

    crud

--- crud/urls.py
+++ crud/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import classroom, classroom_delete, classroom_update, \
-    student, add_student
+    student, add_student, student_detail, student_update
 
 urlpatterns = [
     path("student/", student, name="crud_student"),
     path("add-student/", add_student, name="add_student"),
+    path("student/detail/<int:id>/", student_detail, name="student_detail"),
+    path("student/update/<int:id>/", student_update, name="student_update"),
     path("class/delete/<int:id>/", classroom_delete, name="classroom_delete"),
     path("class/update/<int:id>/", classroom_update, name="classroom_update"),
     path("", classroom, name="crud_classroom")
--- crud/views.py
+++ crud/views.py
@@ -45,11 +45,41 @@ def add_student(request):
         address = request.POST.get("address")
         contact = request.POST.get("contact")
         pp = request.FILES.get('pp')
-        std = Student.objects.create(name=name, email=email, age=age, classroom_id=1)
+        class_id = request.POST.get("class_id")
+        std = Student.objects.create(name=name, email=email, age=age, classroom_id=class_id)
         sp = StudentProfile.objects.create(contact=contact, address=address, student=std)
         if pp:
             sp.profile_picture = pp
             sp.save()
         return redirect("crud_student")
     return render(request, template_name="crud/add_student.html",
-                  context={"title": "Add Student"})
+                  context={"title": "Add Student", "classes": ClassRoom.objects.all()})
+
+
+def student_detail(request, id):
+    std = Student.objects.get(id=id)
+    return render(request, template_name="crud/student_detail.html",
+                  context={"title": "Student Detail", "student": std})
+
+
+def student_update(request, id):
+    std = Student.objects.get(id=id)
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        age = request.POST.get("age")
+        address = request.POST.get("address")
+        contact = request.POST.get("contact")
+        pp = request.FILES.get('pp')
+        Student.objects.filter(id=id).update(name=name, email=email, age=age)
+        sp, _ = StudentProfile.objects.update_or_create(student=std, defaults={"address": address,
+                                                                               "contact": contact})  # (object, True)
+        if pp:
+            sp.profile_picture = pp
+            sp.save()
+        return redirect("student_detail", std.id)
+
+    return render(request, template_name="crud/student_update.html",
+                  context={"title": "Student Update", "student": std})
+
+
